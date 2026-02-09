a74ec40408e730825aabab4e29480b58e816ca69
Shuboi18 <agrawal.shubhay2002@gmail.com>
2026-02-09 14:22:19 +1100

    redo

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 // Use dynamic origin if in production (served by backend), or localhost for dev
-export const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000"
-    : "";
+// export const BASE_URL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:3000"
+//     : "";
+ export const BASE_URL = "http://3.106.248.229:3000";
